d1a20b88b71a67f6d985cd9a81542d44a1a11b4b
amin-aljebbeh <amin.aljebbeh@gmail.com>
2019-03-16 16:19:58 -0400

    update server address

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get('/contractor_customers/:contractor_id', (req, res) => {
   console.log("Fetching user with contractor_id: " + req.params.contractor_id)
 
   const connection = mysql.createConnection({
-    host: "unsummoned-exit.000webhostapp.com",
+    host: "https://databases.000webhost.com/",
     user: "id8992321_amin",
     password: "aminaljebbeh",
     database: "id8992321_contractor"
@@ -51,7 +51,7 @@ app.get('/contractor_customers/:contractor_id/:id', (req, res) => {
   console.log("Fetching user with contractor_id: " + req.params.contractor_id + "customer id " + req.params.id)
 
   const connection = mysql.createConnection({
-    host: "unsummoned-exit.000webhostapp.com",
+    host: "https://databases.000webhost.com/",
     user: "id8992321_amin",
     password: "aminaljebbeh",
     database: "id8992321_contractor"
@@ -85,7 +85,7 @@ app.get('/contractor_customers/:contractor_id/:id', (req, res) => {
 app.post('/addcustomer',jsonParser, (req, res) => {
     console.log(req.body.params)
   const connection = mysql.createConnection({
-    host: "unsummoned-exit.000webhostapp.com",
+    host: "https://databases.000webhost.com/",
     user: "id8992321_amin",
     password: "aminaljebbeh",
     database: "id8992321_contractor"
@@ -119,7 +119,7 @@ app.post('/login', jsonParser, (req, res) => {
 console.log("Fetching user with username: " + req.body.params.username)
 
   const connection = mysql.createConnection({
-    host: "unsummoned-exit.000webhostapp.com",
+    host: "https://databases.000webhost.com/",
     user: "id8992321_amin",
     password: "aminaljebbeh",
     database: "id8992321_contractor"
